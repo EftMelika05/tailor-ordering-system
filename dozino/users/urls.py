8d325864b044bff64b5ca6acb9817a75c78b83b1
Asadi1385 <faeze1385asadi@gmail.com>
2026-06-13 17:13:57 +0330

    add register,login,reset

--- dozino/users/urls.py
+++ dozino/users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    # path('login/',views.login_view , name='login'),
-    path('register/',views.register , name='register')
-    # path('reset/',views.reset_view , name='reset'),
-    # path('profile/',views.profile_view , name='profile')
+    # path('profile/',views.profile_view , name='profile'),
+    path('login/',views.login , name='login'),
+    path('register/',views.register , name='register'),
+    path('reset/',auth_views.PasswordResetView.as_view(template_name='users/reset.html') , name='password_reset')
 ]
